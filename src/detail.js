7ea9db2812ad4fde883c0b276e3b9e53ce9a3c6b
CAI233 <495397430@qq.com>
2019-08-01 10:35:25 +0800

    111

--- src/detail.js
+++ src/detail.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 
 export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+    // constructor(props) {
+    //     super(props);
+    // }
     componentDidMount() {
         console.log(this.props.match.params);
         console.log(this.props);
